4aa0b092eec7d9ce2c8f79f97e64ce7e9789200b
GX_Regent <ivansantagati2009@gmail.com>
2022-04-15 19:45:31 +0200

    Remove all files

--- src/main/java/it/omnisys/plugin/Utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package it.omnisys.plugin.Utils;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtils {
-    public static String color(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-}
